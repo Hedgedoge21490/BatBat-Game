3b84dd56d033e1d0cd4327d0c06a9d66ebeab27e
Hedgedoge21490 <s4joconr@uni-trier.de>
2018-03-19 03:11:58 +0100

    packages lower case, private contructor, diamond operator

---
+++ src/al/artofsoul/batbatgame/entity/batbat/BottomLeftPiece.java
@@ -0,0 +1,47 @@
+package al.artofsoul.batbatgame.entity.batbat;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+import al.artofsoul.batbatgame.entity.MapObject;
+import al.artofsoul.batbatgame.tilemap.TileMap;
+
+/**
+ * @author ArtOfSoul
+ *
+ */
+
+public class BottomLeftPiece extends MapObject {
+	
+	private BufferedImage[] sprites;
+	
+	public BottomLeftPiece(TileMap tm) {
+		super(tm);
+		try {	
+			BufferedImage spritesheet = ImageIO.read(
+				getClass().getResourceAsStream("/Sprites/Other/ballBatBoss.gif")
+			);
+			sprites = new BufferedImage[1];
+			width = height = 4;
+			sprites[0] = spritesheet.getSubimage(0, 10, 10, 10);
+			animation.setFrames(sprites);
+			animation.setDelay(-1);
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update() {
+		x += dx;
+		y += dy;
+		animation.update();
+	}
+	
+	public void draw(Graphics2D g) {
+		super.draw(g);
+	}
+	
+}
